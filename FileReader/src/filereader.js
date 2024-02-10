e956064083c66dcbcd4ec13b9e160f7e4eef516a
Aditya Wanwade <Adiwanwade@gmail.com>
2024-02-10 13:26:45 +0530

    make filereading dynamic

--- FileReader/src/filereader.js
+++ FileReader/src/filereader.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
-fs.readFile(`FileReader/src/poem.txt`, 'utf8', (err, data) => {
+import { getDirName } from './util.js';
+const __dirname = getDirName();
+fs.readFile(`${__dirname}/poem.txt`, 'utf8', (err, data) => {
     if (err) {
         console.error('Error reading the file:', err);
         return;
